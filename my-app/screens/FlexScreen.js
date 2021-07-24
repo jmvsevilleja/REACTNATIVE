cff0a695ce80be25dd6cff98e2190eceb4e721ee
jmvsevilleja <jmvsevilleja@gmail.com>
2021-07-25 06:07:51 +0800

    Added different alignments

--- my-app/screens/FlexScreen.js
+++ my-app/screens/FlexScreen.js
@@ -4,66 +4,81 @@ import {View, Text, ImageBackground, StyleSheet} from 'react-native';
 // React Stateless Function
 function FlexScreen(props) {
   return (
-    <View style={{
-      backgroundColor: "white",
-      flex: 1,
-    }}>
-      {/* <ImageBackground
-                style={style.background}
-                source={require("../../assets/adaptive-icon.png")}
-            >
-            </ImageBackground> */}
+    <View
+      style={{
+        backgroundColor: "grey",
+        flex: 1,
+        // height: 100,
+        flexDirection: "row", // y-axis = column, x-axis = row
+        justifyContent: "center", // flexDirection x/y center axis
+        // justifyContent: "flex-start",// left/top aligned
+        // justifyContent: "flex-end", // right/bottom aligned
+        // justifyContent: "space-around/between/evenly", // spread
+        alignItems: "center", // vertical aligned for row justify per line / not applicable for wrap
+        // alignItems: "flex-end", // bottom aligned for row justify
+        // alignItems: "flex-end", // default top aligned for row justify
+        // alignItems: "baseline", // bottom aligned for the group
+        // alignItems: "stretch", // default top aligned for the group
 
+        // flexWrap: "no-wrap", //default no wrapping per line
+        // flexWrap: "wrap", //wrapping of content / not applicable for alignItems when wrapped
+        // alignContent: "flex-start", // default wrap to flexWrap
+        // alignContent: "center", // Align wrapped content vertically
+        // alignContent: "flex-end", // Bottom aligned for the entire content
+        // alignContent: "space-around", // Spaced
+      }}
+    >
       <View
         style={{
-          backgroundColor: "white",
-          flex: 1,
-          // width: 100,
-          // height: 100,
-          flexDirection: "row", //column,row-reverse
-          justifyContent: "center", // main
-          alignItems: "center", // secondary
-          // alignContent: "center", // applicable to flexWrap
-          //flexWrap: "wrap"
+          backgroundColor: "dodgerblue",
+          // flex: -1, // fill available spaces / no shrink
+          flex: 1, // fill available row/column spaces
+          width: 100,
+          height: 400,
+          // flexShrink: 1, // same as flex -1 / dont shrink
+          // flexBasis: 100, // map for width or height
+          // flexGrow: 1, // same as flex 1, fills the available row/colum spaces
+          // override alignment for individial views
+          // alignSelf: "flex-start", // align this to top
+          // alignSelf: "flex-end", // align this to bottom
+          // alignSelf: "baseline", // align everything on top
+          // alignSelf: "stretch", // fills when no height/width
+          // alignSelf: "auto", // default / parent alignment
+          // alignSelf: "center", // center aligned
 
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "dodgerblue",
-            //flex: 1,
-            width: 100,
-            height: 100,
-            // flexShrink: 1, // same as flex -1
-            // flexBasis: 100, //width or height
-            // flexGrow: 1, // same as flex
-            // alignSelf: "flex-start"
-          }} />
-        <View
-          style={{
-            backgroundColor: "gold",
-            //flex: 1,
-            width: 100,
-            height: 100,
-            position: "relative", // absolute by parent
-            top: -20,
-            left: 20,
-          }} />
-        <View
-          style={{
-            backgroundColor: "red",
-            //flex: 1,
-            width: 100,
-            height: 100,
-          }} />
-        <View
-          style={{
-            backgroundColor: "yellow",
-            //flex: 1,
-            width: 100,
-            height: 100,
-          }} />
-      </View>
+        }} />
+      <View
+        style={{
+          backgroundColor: "yellow",
+          //flex: 1,
+          width: 100,
+          height: 100,
+          // position: "relative", // absolute by parent
+          // top: -20,
+          // left: 20,
+        }} />
+      <View
+        style={{
+          backgroundColor: "red",
+          //flex: 1,
+          width: 100,
+          height: 100,
+        }} />
+      {/* <View
+      style={{
+        backgroundColor: "grey",
+        //flex: 1,
+        width: 100,
+        height: 100,
+      }} />
+    <View
+      style={{
+        backgroundColor: "green",
+        //flex: 1,
+        width: 100,
+        height: 100,
+      }} /> */}
+
     </View>
   );
 }
